aac0427236e83d3b874d438b7cbbc15d689dbe43
Sebastian <sebastian.mofre@gmail.com>
2018-06-19 22:46:00 -0400

    Orden login

--- src/App.js
+++ src/App.js
@@ -7,94 +7,35 @@ import Form from './Form.js'
 
 class App extends Component {
 
-
-  constructor(props) {
-    super(props);
-    this.state = {
-        usuario: []
-    };
-}
-  onSubmitCrear = (fields)=>{
-    console.log("Form_regis",fields);
-  }
-  onSubmit = (fields)=>{
-  console.log("App", fields);
-}
-
-    
-
-  render() {
-    const {usuario} = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bienvenido a Easy Table</h1>
-          <h2 className="App-title">La manera más fácil de reservar</h2>
-          <div className="App-intro">
-</div>
-
-        </header>
-        <br/>
-        <p className="App-intro">
-          <Form onSubmit={fields => this.onSubmit(fields)}/>
-        </p>
-      </div>
-    );
-  }
-
-
-/*
-    state = {
-        fields: {}
-    };
-
-    onChange = updatedValue => {
-        this.setState({
-            fields: {
-                ...this.state.fields,
-                ...updatedValue
-            }
-        });
-    };
-
-    componentDidMount() {
-        fetch('http://localhost:5555')//api.github.com is working
-            .then(response => response.json())
-            .then(responseJSON => {
-                console.log('Respuesta del Backend', responseJSON);
-                
-                
-            })
-            .catch(error => {
-                console.log('Te odio', error);
-            })
-    };
+    constructor(props) {
+	super(props);
+    	this.state = {
+    		usuario: []
+    	};
+    }
 
     render() {
-      return (
-         <div className="App">
-            <header className="App-header">    
-                <h1 className="App-title">Easy Table</h1>
-                <img src={logo} className="App-logo" alt="logo" />
-            </header>  
+    	const {usuario} = this.state;
 
-            <Form onChange={fields => this.onChange(fields)} />
-            <p>
-                {JSON.stringify(this.state.fields, null, 2)}
-            </p>
+    	return (
+            <div className="App">
+    	        <header className="App-header">
+    		        <img src={logo} className="App-logo" alt="logo" />
+    		    	
+    		    	<h1 className="App-title">Bienvenido a Easy Table</h1>
+    		    	<h2 className="App-title">La manera más fácil de reservar</h2>
+    		    	
+    		    	<div className="App-intro">
+                	</div>
 
-         </div>
-        );
-    }*/
-
-
-
-
-
-
-
-
+    		    </header>
+    			<br/>
+	    		<p className="App-intro">
+	    		  	<Form onSubmit={fields => this.onSubmit(fields)}/>
+	    		</p>
+    	    </div>
+    	);
+      }
 
 }
 
--- src/Form.js
+++ src/Form.js
@@ -7,19 +7,17 @@ import urlcodeJson from 'urlcode-json';
 
 export default class Form extends React.Component{
   
-  constructor(props){
-    super(props);
-    this.state = {
-      rut: '',
-      password:  '',
-      loginError: '',
-      url:''
-    };
+    constructor(props){
+        super(props);
+        this.state = {
+            rut: '',
+            password:  '',
+            loginError: '',
+            url:''
+        };
 
-    this.onLoginSubmit = this.onLoginSubmit.bind(this);
-  }
-
-
+        this.onLoginSubmit = this.onLoginSubmit.bind(this);
+    }
 
 //Iniciarsesion
 
@@ -48,53 +46,43 @@ export default class Form extends React.Component{
     }
   }
 
+    render(){
+        return(
+        
+        <div class="Iniciarsesion">
+            <form method="get">
+                <h2> Iniciar sesión </h2>
 
+                <br />
+                RUT <input type ="text"
+                    placeholder="12345678-9" required="required"
+                    value={this.state.rut}
+                    onChange={e => this.setState({rut: e.target.value})}/>
 
+                <br />
+                <br />
 
+                CONTRASEÑA <input type="password"
+                    placeholder="password" required="required"
+                    value={this.state.password}
+                    onChange={e => this.setState({password: e.target.value})}/>
 
+                <br /> 
+                <br /> 
+                <br />
 
-render(){
-  return(
-    //<form  onSubmit={this.onLoginSubmit.bind(this)}>
-    <div class="Iniciarsesion">
-      <form method="post">
-      <h2> Iniciar sesión </h2>
+                <button href="usuario" 
+                    onClick={this.onLoginSubmit} 
+                    class="btn btn-primary btn-block btn-large">Continuar
+                </button>
 
-  <br />
-       RUT <input type ="text"
-      placeholder="12345678-9" required="required"
-      value={this.state.rut}
-      onChange={e => this.setState({rut: e.target.value})}/>
+            </form>
+            <br />
+            <span className="App-sub" >
+                <a href="Form_regis" style={{color: '#000'}} onClick={() => this.CrearUsuario()}>Crear Cuenta</a> 
+            </span>  
+        </div>
 
-  <br />
-  <br />
-      CONTRASEÑA <input type="password"
-      placeholder="password" required="required"
-      value={this.state.password}
-      onChange={e => this.setState({password: e.target.value})}/>
-      <br /> <br /> <br />
-      <button href="usuario" onClick={this.onLoginSubmit} class="btn btn-primary btn-block btn-large">Continuar</button>
-      </form>
-      <br />
-      <span className="App-sub" >
-          <a href="Form_regis" style={{
-              color: '#000'
-          }} onClick={() => this.CrearUsuario()}>Crear Cuenta</a> </span>
-          &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
-      <span className="App-sub" >
-              <a href="recuperar" style={{
-                  color: '#000'
-              }} onClick={() => this.Recuperar_contraseña()}>Recuperar Contraseña</a></span>
-
-
-
-              
-</div>
-
-    /*<input type="submit" value="Continuar"/>
-
-    </form>*/
-
-  );
-}
+        );
+    }
 }
