ff4267707232a767915842edf3d63e054888e07a
Sebastian <sebastian.mofre@gmail.com>
2018-06-20 23:53:37 -0400

    rutas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Sidebar from 'react-sidebar';
 
+import {BrowserRouter as Router, Link} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
+
 import logo from './logo.svg';
 import salty from './salty.gif';
 
@@ -37,24 +40,45 @@ class App extends Component {
     	const {fields, SidebarOpen} = this.state;
 
     	return (
-            <Sidebar sidebar={<b>Sidebar content</b>}
-               open={this.state.sidebarOpen}
-               onSetOpen={this.onSetSidebarOpen}>
-                
-                <div className="App">
-        	        <header className="App-header">
-        		        <img src={logo} className="App-logo" alt="logo" />
-        		    	<h1 className="App-title">Bienvenido a Easy Table</h1>
-        		    	<h2 className="App-title">La manera más fácil de reservar</h2>	
-        		    </header>
-        			<br/>
-        			<h1 className="App-intro">
-                        <Mesas />
-                    </h1>
-                    <br/>
-    	    		
-        	    </div>
-            </Sidebar>    
+            <Router>
+                <Sidebar sidebar={<b>Sidebar content</b>}
+                   open={this.state.sidebarOpen}
+                   onSetOpen={this.onSetSidebarOpen}>
+                    
+                    <div className="App">
+            	        <header className="App-header">
+            		        <img src={logo} className="App-logo" alt="logo" />
+            		    	<h1 className="App-title">Bienvenido a Easy Table</h1>
+            		    	<h2 className="App-title">La manera más fácil de reservar</h2>	
+
+                            <Link to ="/">Login</Link>&emsp;
+                            <Link to ="/registro">Registrarse</Link>&emsp;
+                            <Link to ="/reserva">Reservar</Link>&emsp;
+                            <Link to ="/liberar">Liberar Mesa</Link>&emsp;&emsp;&emsp;&emsp;
+                            <Link to ="/mesas">Mesas</Link>&emsp;
+                            <Link to ="/usuarios">Usuarios</Link>&emsp;
+                            <Link to ="/reservas">Ver Reservas</Link>&emsp;
+
+            		    </header>
+            			<br/>
+                    
+
+
+            			<h1 className="App-intro">
+                            <Route path='/' exact component={Form_login}/>
+                            <Route path='/registro' exact component={Form_registro}/>
+                            <Route path='/reserva' exact component={Form_reserva}/>
+                            <Route path='/liberar' exact component={Form_liberar}/>
+                            <Route path='/mesas' exact component={Mesas}/>
+                            <Route path='/usuarios' exact component={Usuarios}/>
+                            <Route path='/reservas' exact component={Reservas}/>
+                        </h1>
+                        <br/>
+                        
+        	    		
+            	    </div>
+                </Sidebar> 
+            </Router>   
     	);
     }
 
